fc303e47f83121a68183b51a4afaa66aa8503f1b
sy97 <shienyuan97@gmail.com>
2022-04-20 23:46:43 +1200

    update

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Cryptoicon from 'vue-cryptoicon'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import icons from 'vue-cryptoicon/src/icons'
+Cryptoicon.add()
 import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
+app.use(Cryptoicon)
 
 app.use(PrimeVue)
 
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,26 +1,33 @@
 <template>
-    <main class="flex justify-content-center align-items-center">
+    <main class="flex justify-content-center align-items-center flex-column">
         <Card id="convertor" class="w-full">
             <template #content>
                 <div class="py-2">
-                    <small class="block text-gray-300 m-0 mb-2">From</small>
+                    <small class="block text-gray-300 m-0 mb-2">Crypto</small>
                     <div class="p-inputgroup p-inputtext-lg">
                         <InputNumber
                             v-model="from.value"
-                            mode="decimal"
                             :maxFractionDigits="6"
+                            mode="decimal"
+                            class="w-8"
                             placeholder="0"
                         />
 
                         <Dropdown
-                            v-model="from.id"
-                            :options="cryptos"
+                            :virtualScrollerOptions="{ itemSize: 38 }"
+                            v-model="from.currency"
                             :filter="true"
-                            :showClear="true"
-                            optionLabel="name"
-                            optionValue="id"
-                            placeholder="BTC"
-                        />
+                            :options="cryptos"
+                            optionLabel="symbol"
+                            optionValue="symbol"
+                            class="w-4"
+                        >
+                            <template #option="slotProps">
+                                <div class="flex align-items-center">
+                                    <span>{{ slotProps.option.name }}</span>
+                                </div>
+                            </template>
+                        </Dropdown>
                     </div>
                 </div>
 
@@ -31,67 +38,92 @@
                 </div>
 
                 <div class="py-2">
-                    <small class="block text-gray-300 m-0 mb-2">To</small>
+                    <small class="block text-gray-300 m-0 mb-2">Fiat</small>
                     <div class="p-inputgroup p-inputtext-lg">
                         <InputNumber
                             v-model="to.value"
-                            mode="decimal"
                             :maxFractionDigits="6"
+                            class="w-8"
+                            mode="decimal"
                             placeholder="0"
-                            class="border-0"
                         />
 
                         <Dropdown
-                            v-model="to.id"
-                            :options="cryptos"
+                            v-model="to.currency"
                             :filter="true"
-                            :showClear="true"
-                            optionLabel="name"
-                            optionValue="id"
-                            placeholder="USD"
-                        />
+                            :options="fiats"
+                            optionLabel="symbol"
+                            optionValue="symbol"
+                            class="w-4"
+                        >
+                            <template #option="slotProps">
+                                <div class="flex align-items-center">
+                                    <span>{{ slotProps.option.name }}</span>
+                                </div>
+                            </template>
+                        </Dropdown>
                     </div>
                 </div>
 
                 <Button class="w-full mt-5" label="Convert" />
             </template>
         </Card>
+
+        <Card id="result" class="w-full">
+            <template #content>test</template>
+        </Card>
     </main>
 </template>
 
-<script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
-import { Crypto, listCrypto } from '@/api/convert'
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { Currency, listCrypto, listFiat } from '@/api/convert'
 
 import Card from 'primevue/card'
 import InputNumber from 'primevue/inputnumber'
 import Dropdown from 'primevue/dropdown'
 import Button from 'primevue/button'
 
-const from = reactive({
-    id: null,
-    value: null,
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Cryptoicon from 'vue-cryptoicon/src/components/Cryptoicon'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import icons from 'vue-cryptoicon/src/icons'
+Cryptoicon.add(icons)
+
+const from = ref({
+    currency: 'BTC',
+    value: 0,
 })
 
-const to = reactive({
-    id: null,
-    value: null,
+const to = ref({
+    currency: 'USD',
+    value: 0,
 })
 
-const cryptos = ref<Crypto[]>()
+const cryptos = ref<Currency[]>()
+const fiats = ref<Currency[]>()
 const getCryptos = async () => {
     cryptos.value = await listCrypto()
 }
-
+const getFiats = async () => {
+    fiats.value = await listFiat()
+}
 onMounted(async () => {
-    await getCryptos()
+    await Promise.all([getCryptos(), getFiats()])
 })
 </script>
 
 <style lang="scss" scoped>
 #convertor {
-    max-width: 420px;
+    max-width: 520px;
 }
+#result {
+    max-width: 500px;
+    background-color: rgba(7, 20, 38, 0.5);
+}
+
 ::v-deep(.p-card .p-card-content) {
     padding: 0;
 }
