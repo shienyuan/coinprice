e63605ac94bd4251d9eea7907dbd4ef19767ea6f
sy97 <shienyuan97@gmail.com>
2022-05-05 20:40:45 +1200

    refactor functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,88 +2,42 @@ import { fn, db } from './firebase'
 // import dayjs from 'dayjs'
 
 import { cmcRequest } from './cmcapi'
-import { collectionExists } from './utils'
-import { Fiat, CryptoResponse, Crypto } from './types/cmcapi'
+import { Crypto, CryptoResponse, Fiat, FiatResponse } from './types/cmcapi'
 import * as dayjs from 'dayjs'
 
 const statsCol = db.collection('stats')
 const fiatsCol = db.collection('fiats')
 const cryptosCol = db.collection('cryptos')
-// const cryptoUpdateInterval = 1 // day
-const fiatUpdateInterval = 30 // day
+const fetchDataInterval = 30 // day
 
-export const testPopulateFiats = fn.onRequest(async (req, resp) => {
-    const batch = db.batch()
-
-    for (let j = 0; j < 486; j++) {
-        batch.create(fiatsCol.doc(), {})
-    }
-
-    await batch.commit()
-    resp.json((await fiatsCol.get()).docs.length)
-})
-
-export const getFiats = fn.onRequest(async (req, resp) => {
-    const fiats: Fiat[] = []
-
+export const getFiats = fn.onCall(async (): Promise<Fiat[]> => {
     const now = dayjs()
-    const lastUpdatedAt = (await statsCol.doc('fiats').get()).data()
-    console.log(
-        lastUpdatedAt,
-        dayjs(lastUpdatedAt as Date).isAfter(now.add(fiatUpdateInterval, 'day'))
+    const stats = await statsCol.doc('fiats').get()
+    const expired = dayjs(stats.data()?.last_updated_at.toDate()).isAfter(
+        now.add(fetchDataInterval, 'd')
     )
-    const updateCol = false
 
-    const deleteFiats = async () => {
-        const batch = db.batch()
-        const existingFiats = await fiatsCol.get()
-        if (existingFiats.size === 0) {
-            return
-        }
-
-        if (existingFiats.size <= 500) {
-            existingFiats.docs.forEach((val) => batch.delete(val.ref))
-            await batch.commit()
-            return
-        } else {
-            for (let i = 0; i < 500; i++) {
-                batch.delete(existingFiats.docs[i].ref)
-            }
-        }
-
-        await batch.commit()
-        await deleteFiats()
+    if (!stats.exists || expired) {
+        return await updateFiats(now.toDate())
     }
 
-    if (!lastUpdatedAt || updateCol) {
-        await deleteFiats()
-    }
-
-    resp.status(200).json(fiats)
+    const snaps = await fiatsCol.get()
+    return snaps.docs.map((doc) => doc.data() as Fiat)
 })
 
-export const getCryptos = fn.onRequest(async (req, resp) => {
-    let updateCol = false
-    let cryptos: Crypto[] = []
+export const getCryptos = fn.onCall(async (): Promise<Crypto[]> => {
+    const now = dayjs()
+    const stats = await statsCol.doc('cryptos').get()
+    const expired = dayjs(stats.data()?.last_updated_at.toDate()).isAfter(
+        now.add(fetchDataInterval, 'd')
+    )
 
-    if (!collectionExists(cryptosCol)) {
-        const { data } = await cmcRequest.get<CryptoResponse>(
-            '/v1/cryptocurrency/map'
-        )
-        cryptos = data.data
-        updateCol = true
+    if (!stats.exists || expired) {
+        return await updateCryptos(now.toDate())
     }
 
-    if (updateCol) {
-        const batch = db.batch()
-        for (let i = 0; i < cryptos.length; i += 500) {
-            cryptos.forEach((val) => batch.set(cryptosCol.doc(), val))
-            await batch.commit()
-        }
-        await statsCol.doc('cryptos').set({ last_updated: dayjs().toDate() })
-    }
-
-    resp.status(200).json(cryptos)
+    const snaps = await cryptosCol.get()
+    return snaps.docs.map((doc) => doc.data() as Crypto)
 })
 
 export const convertCurrency = fn.onCall(async (req: any) => {
@@ -98,12 +52,42 @@ export const convertCurrency = fn.onCall(async (req: any) => {
     return converted.data
 })
 
-export const listCurrencies = fn.onCall(async (req) => {
-    let url = '/v1/cryptocurrency/map'
-    if (req.type === 'fiat') {
-        url = '/v1/fiat/map'
-    }
+const updateFiats = async (now: Date): Promise<Fiat[]> => {
+    const { data } = await cmcRequest.get<FiatResponse>('/v1/fiat/map')
+    const batch = db.batch()
 
-    const { data } = await cmcRequest.get(url)
-    return data
-})
+    data.data.forEach((fiat) =>
+        batch.set(fiatsCol.doc(fiat.id.toString()), fiat)
+    )
+
+    await batch.commit()
+    await statsCol
+        .doc('fiats')
+        .set({ last_updated_at: now, total: data.data.length })
+
+    return data.data
+}
+
+const updateCryptos = async (now: Date): Promise<Crypto[]> => {
+    const { data } = await cmcRequest.get<CryptoResponse>(
+        '/v1/cryptocurrency/map',
+        {
+            params: {
+                limit: 500,
+                sort: 'cmc_rank',
+            },
+        }
+    )
+    const batch = db.batch()
+
+    data.data.forEach((fiat) =>
+        batch.set(cryptosCol.doc(fiat.id.toString()), fiat)
+    )
+
+    await batch.commit()
+    await statsCol
+        .doc('cryptos')
+        .set({ last_updated_at: now, total: data.data.length })
+
+    return data.data
+}
