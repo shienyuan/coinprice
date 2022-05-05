85bbd78c0711c092af94ef73954a6c770e444fdf
sy97 <shienyuan97@gmail.com>
2022-05-05 16:34:18 +1200

    refactor functions

---
+++ functions/src/types/cmcapi.ts
@@ -0,0 +1,43 @@
+export interface Status {
+    timestamp: Date
+    errorCode: number
+    errorMessage: null
+    elapsed: number
+    creditCount: number
+    notice: null
+}
+
+export interface FiatResponse {
+    status: Status
+    data: Fiat[]
+}
+
+export interface CryptoResponse {
+    status: Status
+    data: Crypto[]
+}
+
+export interface Fiat {
+    id: number
+    name: string
+    sign: string
+    symbol: string
+}
+
+export interface Crypto {
+    id: number
+    rank: number
+    name: string
+    symbol: string
+    slug: string
+    isActive: number
+    firstHistoricalData: Date
+    lastHistoricalData: Date
+    platform: {
+        id: number
+        name: string
+        symbol: string
+        slug: string
+        tokenAddress: string
+    } | null
+}
