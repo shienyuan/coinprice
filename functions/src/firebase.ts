0df9ca6c9d1a06a0e13f7f36e15d32d41f4bd641
sy97 <shienyuan97@gmail.com>
2022-05-09 23:28:02 +1200

    v0.4.0

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -7,6 +7,7 @@ const dev = process.env.ENV === 'development'
 
 export const fs = admin.firestore()
 export const fn = functions.https
+export const HttpError = functions.https.HttpsError
 export const db = {
     statsCol: fs.collection(`stats${dev ? '_dev' : ''}`),
     fiatsCol: fs.collection(`fiats${dev ? '_dev' : ''}`),
