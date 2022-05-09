2d2ce720b6a4e18b30da60079a78403c49b68474
sy97 <shienyuan97@gmail.com>
2022-05-10 00:20:56 +1200

    v0.4.0

---
+++ app/src/plugins/logrocket.ts
@@ -0,0 +1,16 @@
+import * as LogRocket from 'logrocket'
+import { analyticsLogEvent } from '@/plugins/firebase'
+
+export default {
+    install: (): void => {
+        if (process.env.NODE_ENV === 'development') return
+        LogRocket.init('***REMOVED***')
+        LogRocket.getSessionURL(function (sessionURL) {
+            analyticsLogEvent('send', {
+                hitType: 'event',
+                eventCategory: 'LogRocket',
+                eventAction: sessionURL,
+            })
+        })
+    },
+}
